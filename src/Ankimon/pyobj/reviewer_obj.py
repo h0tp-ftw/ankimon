2371e10acf68d12c46dba53066bc74b3042e1909
Nut2026 <256511636+Nut2026@users.noreply.github.com>
2026-08-28 15:04:41 +0800

    move: styling in reviewer to hud styling

--- src/Ankimon/pyobj/reviewer_obj.py
+++ src/Ankimon/pyobj/reviewer_obj.py
@@ -282,7 +282,7 @@ class Reviewer_Manager:
             self.settings.get("misc.language"),
             _boost_snapshot(self.enemy_pokemon),
             _boost_snapshot(self.main_pokemon),
-            self.settings.get("gui.styling_in_reviewer"),
+            self.settings.get("gui.hud_styling", True),
             self.settings.get("gui.hud_player_sprite"),
             self.settings.get("gui.hud_enemy_sprite"),
             self.settings.get("gui.hud_xp_bar"),
@@ -446,7 +446,7 @@ class Reviewer_Manager:
         hud_html += "</div>"
 
         # Build hud_css
-        if self.settings.get("gui.styling_in_reviewer"):
+        if self.settings.get("gui.hud_styling", True):
             hud_css = create_css_for_reviewer(
                 int(self.settings.get("gui.show_mainpkmn_in_reviewer")),
                 pokemon_hp_percent,
